203a11d792974aa6cd7f0c45d15f51c216578eda
petrus <pshai671@gmail.com>
2023-01-24 12:19:55 +0200

    Add project files.

---
+++ Learners.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+     class Learners
+    {
+        public string firstname;
+        public string lastname;
+        public string gender;
+        public char initial;
+        public int age;
+
+        public Learners(string Firstname, string Lastname, string Gender, char Initial, int Age)
+        {
+            firstname = Firstname;
+            lastname = Lastname;
+            gender = Gender;
+            initial = Initial;
+            age = Age;
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Learners learner1 = new Learners("Malesela", "Letsoalo", "Male", 'J', 30);
+        Learners learner2 = new Learners("Petrus", "Shai", "Male", 'P', 24);
+        Learners learner3 = new Learners("Precious", "Mpuru", "Female", 'M', 23);
+        Learners learner4 = new Learners("Thuli", "Baloyi", "Female", 'J', 24);
+        Console.WriteLine(learner1.firstname + " " + learner1.lastname + " " + learner1.initial + " " + learner1.gender + " " + learner1.age);
+        Console.WriteLine(learner2.firstname + " " + learner2.lastname + " " + learner2.initial + " " + learner2.gender + " " + learner2.age);
+        Console.WriteLine(learner3.firstname + " " + learner3.lastname + " " + learner3.initial + " " + learner3.gender + " " + learner3.age);
+        Console.WriteLine(learner4.firstname + " " + learner4.lastname + " " + learner4.initial + " " + learner4.gender + " " + learner4.age);
+
+    }
+
+}
